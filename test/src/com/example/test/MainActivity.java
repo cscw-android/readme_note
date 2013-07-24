bdef0b6d05bbf8d64fcb16899a9a20f969d0d943
cscwhw <767011410@qq.com>
2013-07-24 21:46:16 +0800

    test

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        System.out.println("222222");
         return true;
+       
     }
     
 }
